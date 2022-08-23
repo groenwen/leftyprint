fbc1af547fa242194f010b4ccc2bfa1a00a4a895
yawen <S2004001@samebest.com.tw>
2022-08-23 15:54:13 +0800

    add dist

---
+++ dist/js/381.4b1ffc95.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkleftyprint"]=self["webpackChunkleftyprint"]||[]).push([[381],{7381:function(n,t,e){e.r(t),e.d(t,{default:function(){return o}});var r=e(3396);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),s=[a];function i(n,t){return(0,r.wg)(),(0,r.iD)("div",u,s)}var c=e(89);const f={},l=(0,c.Z)(f,[["render",i]]);var o=l}}]);
+//# sourceMappingURL=381.4b1ffc95.js.map
